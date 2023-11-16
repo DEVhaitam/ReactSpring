2e6f02881594737c4bd22cea1854a69214bf05d0
haitam <haitamelhayanitech@gmail.com>
2023-11-16 02:50:45 +0100

    first commit

---
+++ spring-backend/src/test/java/com/reactspring/crudproject/CrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactspring.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
