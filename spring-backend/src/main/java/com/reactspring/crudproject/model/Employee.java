3352a0f994befa3964bcef98eec3396a572f3ae9
haitam <haitamelhayanitech@gmail.com>
2023-11-19 00:07:42 +0100

    model, controller, repository added

--- spring-backend/src/main/java/com/reactspring/crudproject/model/Employee.java
+++ spring-backend/src/main/java/com/reactspring/crudproject/model/Employee.java
@@ -1,6 +1,6 @@
 package com.reactspring.crudproject.model;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +9,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table
+@Table(name = "employees")
 public class Employee {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email_id")
     private String emailId;
 
 }
---
+++ spring-backend/src/main/java/com/reactspring/crudproject/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.reactspring.crudproject.repository;
+
+import com.reactspring.crudproject.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
